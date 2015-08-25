50fb78288ab3cb82779784349edf6b4323fe7797
mrjaimisra <misrajai01@gmail.com>
2015-08-25 09:45:58 -0600

    add validations for name presence and url presence, and created a generate_url method to parameterize the name

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,7 +2,9 @@ class Store < ActiveRecord::Base
   validates :name, presence: true
   validates :url,  presence: true, uniqueness: true
 
+  before_validation :generate_url
+  
   def generate_url
-    
+    self.url = name.parameterize
   end
 end
