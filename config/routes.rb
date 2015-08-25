3ffd0c5d2845095b1e95618f236b860855265cb5
mrjaimisra <misrajai01@gmail.com>
2015-08-25 11:48:43 -0600

    remove unnecessary routes, set a get '/' route to the stores/items#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   root 'stores#index'
 
-  resources :items, only: [:index, :show]
-  resources :orders, only: [:index, :show]
-  resources :users, only: [:index, :show]
-
   namespace :stores, path: ':store', as: :store do
+    get '/', to: 'items#index'
     resources :items, only: [:index, :show]
     resources :orders, only: [:index, :show]
   end
