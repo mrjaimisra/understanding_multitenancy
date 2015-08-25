e9d23c41b3b82a2d2a563b0f6b56e0f9d8963a6a
mrjaimisra <misrajai01@gmail.com>
2015-08-25 10:58:41 -0600

    extract current store to stores/stores_controller, add redirect if store not found as before action in stores/stores_controller

--- app/controllers/stores/items_controller.rb
+++ app/controllers/stores/items_controller.rb
@@ -1,4 +1,4 @@
-class Stores::ItemsController < ApplicationController
+class Stores::ItemsController < Stores::StoresController
   def index
     @items = current_store.items
   end
@@ -7,12 +7,5 @@ class Stores::ItemsController < ApplicationController
     @item = current_store.items.find_by(id: params[:id])
 
     redirect_to store_items_path(store: current_store.url), notice: "Item not found." if @item.nil?
-  end
-
-  def private
-
-    def current_store
-      @current_store ||= Store.find_by(url: params[:store])
-    end
   end
 end
---
+++ app/controllers/stores/stores_controller.rb
@@ -0,0 +1,13 @@
+class Stores::StoresController < ApplicationController
+  helper_method :current_store
+
+  before_action :store_not_found
+
+  def current_store
+    @current_store ||= Store.find_by(url: params[:store])
+  end
+
+  def store_not_found
+    redirect_to root_path if current_store.nil?
+  end
+end
