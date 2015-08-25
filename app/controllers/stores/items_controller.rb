19702ffde75facca21c824603641cfd9c740e739
mrjaimisra <misrajai01@gmail.com>
2015-08-25 10:48:18 -0600

    add private method to stores/items_controller to find the current_store

--- app/controllers/stores/items_controller.rb
+++ app/controllers/stores/items_controller.rb
@@ -1,13 +1,18 @@
 class Stores::ItemsController < ApplicationController
   def index
-    store = Store.find_by(url: params[:store])
-    @items = store.items
+    @items = current_store.items
   end
 
   def show
-    store = Store.find_by(url: params[:store])
-    @item = store.items.find_by(id: params[:id])
+    @item = current_store.items.find_by(id: params[:id])
 
-    redirect_to store_items_path(store: store.url) if @item.nil?
+    redirect_to store_items_path(store: current_store.url), notice: "Item not found." if @item.nil?
+  end
+
+  def private
+
+    def current_store
+      @current_store ||= Store.find_by(url: params[:store])
+    end
   end
 end
