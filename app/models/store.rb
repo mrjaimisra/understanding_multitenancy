d159f660bbf17774f3e456a883c2533dce86b38d
mrjaimisra <misrajai01@gmail.com>
2015-08-25 09:54:53 -0600

    set up a one-to-many relationship between stores and items - store has many items

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,6 +2,8 @@ class Store < ActiveRecord::Base
   validates :name, presence: true
   validates :url,  presence: true, uniqueness: true
 
+  has_many :items
+
   before_validation :generate_url
 
   def generate_url
